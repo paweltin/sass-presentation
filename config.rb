3e582b81d7361b5736d967801a13cfa41b0dc36b
Pawel <login.kowalewski@gmail.com>
2014-03-27 04:00:45 +0100

    Almost done

---
+++ config.rb
@@ -0,0 +1,15 @@
+# COMPASS SETTINGS
+http_path = "" # use if other than /
+sass_dir = "scss"
+css_dir = "css"
+images_dir = "img"
+javascripts_dir = "js"
+
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
